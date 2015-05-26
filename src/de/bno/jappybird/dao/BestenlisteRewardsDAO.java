6405a9f9434892e36d647ce46b4d6b300a65f01e
Marvin Bruns <android.bruns@googlemail.com>
2015-05-26 07:52:43 +0200

    reward client

--- src/de/bno/jappybird/dao/BestenlisteRewardsDAO.java
+++ src/de/bno/jappybird/dao/BestenlisteRewardsDAO.java
@@ -1,6 +1,12 @@
 package de.bno.jappybird.dao;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import de.bno.jappybird.settings.HSettings;
+import de.kvwl.n8dA.infrastructure.commons.entity.GamePerson;
+import de.kvwl.n8dA.infrastructure.rewards.client.CreditAccessClient;
+import de.kvwl.n8dA.infrastructure.rewards.client.ExceptionDialog;
 
 public class BestenlisteRewardsDAO implements BestenlisteDAO
 {
@@ -8,22 +14,84 @@ public class BestenlisteRewardsDAO implements BestenlisteDAO
 	@Override
 	public List<Score> getBestenliste()
 	{
-		// TODO Auto-generated method stub
-		return null;
+
+		List<Score> highscore = new LinkedList<Score>();
+
+		String gameName = HSettings.getString(HSettings.KEY_GAME_NAME, "JappyBird");
+		CreditAccessClient accessClient = null;
+		try
+		{
+			accessClient = new CreditAccessClient(
+				HSettings.getString(HSettings.KEY_REWARD_SERVER_ADDRESS, "localhost"), HSettings.getBoolean(
+					HSettings.KEY_INSTALL_SECURITY_MANAGER, false));
+			accessClient.initConnectionToServer();
+
+			List<GamePerson> first10Scores = accessClient.getFirst10GamePersonsForGame(gameName);
+
+			int count = 0;
+			for (GamePerson person : first10Scores)
+			{
+				if (count >= 10)
+				{
+					break;
+				}
+
+				highscore.add(new Score(person.getPerson().getName(), person.getPoints()));
+
+				count++;
+			}
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return highscore;
 	}
 
 	@Override
 	public boolean insertScore(Score score)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		boolean isInHighscore = false;
+
+		String gameName = HSettings.getString(HSettings.KEY_GAME_NAME, "JappyBird");
+
+		CreditAccessClient accessClient = null;
+		try
+		{
+			accessClient = new CreditAccessClient(
+				HSettings.getString(HSettings.KEY_REWARD_SERVER_ADDRESS, "localhost"), HSettings.getBoolean(
+					HSettings.KEY_INSTALL_SECURITY_MANAGER, false));
+			accessClient.initConnectionToServer();
+
+			List<GamePerson> first10Scores = accessClient.getFirst10GamePersonsForGame(gameName);
+			GamePerson lastScore = first10Scores.get(Math.min(first10Scores.size() - 1, 9));
+			Integer lastPoints = lastScore.getPoints();
+
+			if (first10Scores.size() < 10 || score.getScore() > lastPoints)
+			{
+
+				isInHighscore = true;
+			}
+
+			accessClient.persistConfigurationPointsForPerson(score.getName(), gameName, score.getScore());
+		}
+		catch (Exception e)
+		{
+			ExceptionDialog.showExceptionDialog(null, "Verbindungsfehler",
+				"Es konnte keien Verbindung zum Punkteserver aufgebaut werden.", e);
+			return false;
+		}
+
+		return isInHighscore;
 	}
 
 	@Override
 	public boolean isHighscore(int score)
 	{
-		// TODO Auto-generated method stub
-		return false;
+
+		return true;
 	}
 
 }

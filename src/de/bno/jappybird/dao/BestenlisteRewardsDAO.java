e08a9a8f7fe506e01e72e3358aee80147096edc5
Marvin Bruns <android.bruns@googlemail.com>
2015-05-26 07:31:26 +0200

    dao

---
+++ src/de/bno/jappybird/dao/BestenlisteRewardsDAO.java
@@ -0,0 +1,29 @@
+package de.bno.jappybird.dao;
+
+import java.util.List;
+
+public class BestenlisteRewardsDAO implements BestenlisteDAO
+{
+
+	@Override
+	public List<Score> getBestenliste()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertScore(Score score)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isHighscore(int score)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
